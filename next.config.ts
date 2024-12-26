2bb2d24669c037af42d78169df5fd2ac4c30561a
Malavisto <malavistobusiness@gmail.com>
2024-12-26 23:43:45 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/anilist-randomizer', // Replace with your repository name
+  basePath: '/', // Replace with your repository name
 }
 
 export default nextConfig
